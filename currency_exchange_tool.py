b2a5528c7cb2f5c1b1313c13cb1ba77d688bb37e
Tonye26 <tonye.campbell@laetottenham.ac.uk>
2024-10-15 10:53:02 +0000

    Pending changes exported from your codespace

--- currency_exchange_tool.py
+++ currency_exchange_tool.py
@@ -3,11 +3,18 @@ import math # you'll probably need this
 exchange_rates = {
     'USD': 1.13, #I.E. 1 Pound is 1.13 Dollars
     'EUR': 1.15,
+    'CAD': 1.80,
+    'LEK': 118.00,
+    'VND': 324220.09,
 }
 
 def check_currency_exists(currency):
     return
 
-def currency_convert(original_c, new_c, amount):
-    # your code here
-    return
+def currency_convert(new_c, amount):
+    conversion_rate=exchange_rates.get(new_c)
+    total=amount*conversion_rate
+    total=round(total,2)
+    print(total)
+    return total
+
