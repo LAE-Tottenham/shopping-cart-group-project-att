885161a54c50f33fa01fbb7b2ac52aee79ed6886
Tonylin979 <tony.lin@laetottenham.ac.uk>
2024-10-18 10:47:33 +0000

    Pending changes exported from your codespace

--- currency_exchange_tool.py
+++ currency_exchange_tool.py
@@ -3,11 +3,21 @@ import math # you'll probably need this
 exchange_rates = {
     'USD': 1.13, #I.E. 1 Pound is 1.13 Dollars
     'EUR': 1.15,
+    "ALL": 118.39,
+    "VND": 32617.15,
+    
 }
 
-def check_currency_exists(currency):
-    return
-
+def check_currency_exists():
+    currency = input("Choose a desired currency(USD,EUR,CAD,ALL,VND): ")
+    if not currency.isalpha():
+        print(" Invalid format ")
+    if currency.upper() in exchange_rates:
+        return (f"{currency.upper()}")
+    else:
+        print ("Currency unavailable")
+    
+check_currency_exists()    
 def currency_convert(original_c, new_c, amount):
     # your code here
     return
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from currency_exchange_tool import currency_convert
 from shop_functions import start_shop
-
+import pgeocode
+import math
 print('Welcome to my shop')
+import postcodes_uk
+print(postcodes_uk.validate("N17 0BX"))
 
-while True:
-    print('Please select what you would like to buy')
-    items_to_buy = start_shop()
+def calculate_distance(postcode1, postcode2):
+    nomi = pgeocode.Nominatim('GB')
+    
+    distance_km = pgeocode.GeoDistance('GB').query_postal_code(postcode1, postcode2)
+    
+    return distance_km
 
-    # blah blah 
+postcode1 = "N17 0BX"
+postcode2 = "dsfsdf"
+distance = calculate_distance(postcode1, postcode2)
+print(distance)
+
